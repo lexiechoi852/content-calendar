289fbe1cd5403781a15b7e27c43276340d183e85
Lexie Choi <lexiechoi852@gmail.com>
2024-01-06 05:15:45 -0500

    feat: added content respository and data loader using json file

--- src/main/java/com/example/contentcalendar/model/Content.java
+++ src/main/java/com/example/contentcalendar/model/Content.java
@@ -1,13 +1,17 @@
 package com.example.contentcalendar.model;
 
 import jakarta.validation.constraints.NotBlank;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 
 import java.time.LocalDateTime;
 
 public record Content(
+        @Id
         Integer id,
         @NotBlank
         String title,
+        @Column(value = "description")
         String desc,
         Status status,
         Type contentType,
