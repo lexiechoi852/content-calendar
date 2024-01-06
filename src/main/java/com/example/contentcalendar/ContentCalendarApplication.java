18c360aec19279a7a4b4b1c0e936c64f1d5886a6
Lexie Choi <lexiechoi852@gmail.com>
2024-01-06 05:16:18 -0500

    feat: added configuration properties

--- src/main/java/com/example/contentcalendar/ContentCalendarApplication.java
+++ src/main/java/com/example/contentcalendar/ContentCalendarApplication.java
@@ -1,13 +1,16 @@
 package com.example.contentcalendar;
 
+import com.example.contentcalendar.config.ContentCalendarProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ContentCalendarProperties.class)
 public class ContentCalendarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContentCalendarApplication.class, args);
 	}
-
+	
 }
